5767c02d849c7ad909bf228cda156e71d2032c12
child9527 <31912488+child9527@users.noreply.github.com>
2019-01-22 00:00:31 +0800

    Update k2p_brc_tmp.sh

--- k2p_brc_tmp.sh
+++ k2p_brc_tmp.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2018 autoget-SS
 # Copyright (C) 2018 child9527 <child9527@qq.com>
 #
-wget --no-check-certificate --timeout=60 -qO /root/chkss.sh https://raw.githubusercontent.com/child9527/getvpn/master/k2p_brc_chkss.sh
-chmod 777 /root/chkss.sh
-/bin/sh /root/chkss.sh 2>/dev/null
-sed -i 1i\ '17 */3 * * * /bin/sh /root/chkss.sh 2>/dev/null' /tmp/media/data/cron_file
-sed -i 1i\ '/bin/sh /root/chkv2.sh 2>/dev/null' /tmp/media/data/auto_file
+wget --no-check-certificate --timeout=60 -qO /tmp/chkss.sh https://raw.githubusercontent.com/child9527/getvpn/master/k2p_brc_chkss.sh
+chmod 777 /tmp/chkss.sh
+/bin/sh /tmp/chkss.sh 2>/dev/null
+sed -i 1i\ '17 */3 * * * /bin/sh /tmp/chkss.sh 2>/dev/null' /tmp/media/data/cron_file
+sed -i 1i\ '/bin/sh /tmp/chkv2.sh 2>/dev/null' /tmp/media/data/auto_file
