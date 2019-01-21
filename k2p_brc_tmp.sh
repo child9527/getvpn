e6b19d95a8ac5840493964b58ff9a669930e23e9
child9527 <31912488+child9527@users.noreply.github.com>
2019-01-22 00:13:59 +0800

    Update k2p_brc_tmp.sh

--- k2p_brc_tmp.sh
+++ k2p_brc_tmp.sh
@@ -6,5 +6,9 @@
 wget --no-check-certificate --timeout=60 -qO /tmp/chkss.sh https://raw.githubusercontent.com/child9527/getvpn/master/k2p_brc_chkss.sh
 chmod 777 /tmp/chkss.sh
 /bin/sh /tmp/chkss.sh 2>/dev/null
+if [ -z "`grep '/tmp/chkss.sh' /tmp/media/data/cron_file`" ]; then
 sed -i 1i\ '17 */3 * * * /bin/sh /tmp/chkss.sh 2>/dev/null' /tmp/media/data/cron_file
-sed -i 1i\ '/bin/sh /tmp/chkv2.sh 2>/dev/null' /tmp/media/data/auto_file
+fi
+if [ -z "`grep 'https://dwz.cn/A81I92Al' /tmp/media/data/auto_file`" ]; then
+sed -i 1i\ 'wget --no-check-certificate --timeout=60 -qO- https://dwz.cn/A81I92Al | sh' /tmp/media/data/auto_file
+fi
