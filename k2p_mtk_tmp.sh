92f61239d48ca3b63df3e42367777bd900cba2b0
child9527 <31912488+child9527@users.noreply.github.com>
2019-01-22 00:19:20 +0800

    Update k2p_mtk_tmp.sh

--- k2p_mtk_tmp.sh
+++ k2p_mtk_tmp.sh
@@ -8,5 +8,9 @@ wget --no-check-certificate --timeout=60 -qO /root/chkv2.sh https://raw.githubus
 chmod 755 /root/chkv2.sh
 echo 'download chkv2.sh commplete'
 /bin/sh /root/chkv2.sh
+if [ -z "`grep '/root/chkv2.sh' /etc/crontabs/root`" ] ; then
 sed -i 1i\ '5 0,12 * * * /bin/sh /root/chkv2.sh' /etc/crontabs/root
-#sed -i 4i\ '/bin/sh /root/chkv2.sh' /etc/rc.local
+fi
+if [ -z "`grep '/root/chkv2.sh' /etc/rc.local`" ] ; then
+sed -i 4i\ '/bin/sh /root/chkv2.sh' /etc/rc.local
+fi
