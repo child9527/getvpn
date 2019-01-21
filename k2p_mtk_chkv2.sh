bf611abfddf404f7e58b34ef0b40a41d1760cc68
child9527 <31912488+child9527@users.noreply.github.com>
2019-01-21 21:19:14 +0800

    Update k2p_mtk_chkv2.sh

--- k2p_mtk_chkv2.sh
+++ k2p_mtk_chkv2.sh
@@ -3,30 +3,6 @@
 # Copyright (C) 2018 child9527 <child9527@qq.com>
 # This is free script, for help you get free v2ray account to break GFW.
 
-setv2( ) {
-uci set shadowsocksr.cfg024a8f='servers'
-uci set shadowsocksr.cfg024a8f.v2type='0'
-uci set shadowsocksr.cfg024a8f.protocol='origin'
-uci set shadowsocksr.cfg024a8f.obfs='plain'
-uci set shadowsocksr.cfg024a8f.timeout='60'
-uci set shadowsocksr.cfg024a8f.password='helloworld'
-uci set shadowsocksr.cfg024a8f.v2level='0'
-uci set shadowsocksr.cfg024a8f.encrypt_method='rc4-md5'
-uci set shadowsocksr.cfg024a8f.ssencrypt='rc4-md5'
-uci set shadowsocksr.cfg024a8f.alias='freev2.org'
-uci set shadowsocksr.cfg024a8f.v2encrypt='none'
-uci set shadowsocksr.cfg024a8f.type='2'
-uci set shadowsocksr.global.server_index='1'
-uci set shadowsocksr.global.global_server='cfg024a8f'
-uci set shadowsocksr.cfg024a8f.server=$1
-uci set shadowsocksr.cfg024a8f.v2alertid=$4
-uci set shadowsocksr.cfg024a8f.server_port=$2
-uci set shadowsocksr.cfg024a8f.v2uuid=$3
-uci commit shadowsocksr
-sleep 3
-/etc/init.d/shadowsocksr restart
-}
-
 cfg=`uci get shadowsocksr.global.global_server`
 if [ "$cfg" == "nil" ] ;then
 	wget --no-check-certificate --timeout=60 -qO /tmp/wy.txt http://freev2.org
@@ -35,7 +11,15 @@ if [ "$cfg" == "nil" ] ;then
 	rport=`cat /tmp/wy.txt | grep '"port"'| sed s/[[:space:]]//g | cut -d'=' -f2 | cut -d'>' -f2`
 	ruuid=`cat /tmp/wy.txt | grep UUID | cut -d'=' -f2 | cut -d'>' -f2`
 	rAlertid=`cat /tmp/wy.txt | grep AlterID |sed s/[[:space:]]//g | cut -d':' -f2 | cut -d'<' -f1`
-	setv2 $rip $rport $ruuid $rAlertid 2>/dev/null
+	uci set shadowsocksr.cfg024a8f.type='2'
+	uci set shadowsocksr.global.global_server='cfg024a8f'
+	uci set shadowsocksr.cfg024a8f.server=$rip
+	uci set shadowsocksr.cfg024a8f.v2alertid=$rAlertid
+	uci set shadowsocksr.cfg024a8f.server_port=$rport
+	uci set shadowsocksr.cfg024a8f.v2uuid=$ruuid
+	uci commit shadowsocksr
+	uci set shadowsocksr.global.server_index='1'
+	/etc/init.d/shadowsocksr restart
 	echo 'VPN has not found,execute V2ray'
 	exit 0
 	fi
@@ -57,7 +41,15 @@ if [ "$?" == "0" ]; then
 	rAlertid=`cat /tmp/wy.txt | grep AlterID |sed s/[[:space:]]//g | cut -d':' -f2 | cut -d'<' -f1`
 		if [[ "$luuid" != "$ruuid" ]] || [[ "$lip" != "$rip" ]] || [[ "$lport" != "$rport" ]] || [[ "$lAlertid" != "$rAlertid" ]]; then
 		echo 'config changed,execute reconfig V2ray'
-		setv2 $rip $rport $ruuid $rAlertid 2>/dev/null
+		uci set shadowsocksr.cfg024a8f.type='2'
+		uci set shadowsocksr.global.global_server='cfg024a8f'
+		uci set shadowsocksr.cfg024a8f.server=$rip
+		uci set shadowsocksr.cfg024a8f.v2alertid=$rAlertid
+		uci set shadowsocksr.cfg024a8f.server_port=$rport
+		uci set shadowsocksr.cfg024a8f.v2uuid=$ruuid
+		uci commit shadowsocksr
+		uci set shadowsocksr.global.server_index='1'
+		/etc/init.d/shadowsocksr restart
 		else
 		echo 'config has not change'
 		fi
