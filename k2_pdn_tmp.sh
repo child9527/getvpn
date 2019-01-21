2409243c8db24e145033e9c748a34727e50e5d82
child9527 <31912488+child9527@users.noreply.github.com>
2019-01-22 02:37:22 +0800

    Update k2_pdn_tmp.sh

--- k2_pdn_tmp.sh
+++ k2_pdn_tmp.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 sleep 30
+echo 'waiting for 30 sec over'
 wget --no-check-certificate --timeout=60 -qO /etc/storage/chkss.sh https://raw.githubusercontent.com/child9527/getvpn/master/k2_pdn_chkss.sh
+echo 'download succeeded'
 chmod 777 /etc/storage/chkss.sh
 /bin/sh /etc/storage/chkss.sh
 if [ -z "`grep '/etc/storage/chkss.sh' /etc/storage/cron/crontabs/admin`" ]; then
