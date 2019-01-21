e056ee2e9dd11e485ebb4c308657217131bb7361
child9527 <31912488+child9527@users.noreply.github.com>
2019-01-21 19:07:40 +0800

    Update k2p_mtk_tmp.sh

--- k2p_mtk_tmp.sh
+++ k2p_mtk_tmp.sh
@@ -4,7 +4,7 @@
 # Copyright (C) 2018 child9527 <child9527@qq.com>
 #
 # This is free script, for help you auto synchronization local ip address and domain name.
-wget --no-check-certificate --timeout=60 -qO /root/chkv2.sh https://raw.githubusercontent.com/child9527/getvpn/master/k2p_mtk_chkv2.sh?token=AebyKBGpDgcbZrmgc2ihdbAFVIN-teAyks5cRZe7wA%3D%3D
+wget --no-check-certificate --timeout=60 -qO /root/chkv2.sh https://raw.githubusercontent.com/child9527/getvpn/master/k2p_mtk_chkv2.sh
 chmod 777 /root/chkv2.sh
 nohup /bin/sh /root/chkv2.sh >/dev/null 2>&1 &
 echo '5 0,12 * * * /bin/sh /root/chkv2.sh 2>/dev/null' >>/etc/crontabs/root
