9a6f1031486edb96fbe0922f2d63e1ec95f8893c
child9527 <31912488+child9527@users.noreply.github.com>
2019-01-23 02:46:31 +0800

    Add files via upload

--- k3_tmp.sh
+++ k3_tmp.sh
@@ -4,11 +4,17 @@
 # Copyright (C) 2018 child9527 <child9527@qq.com>
 #
 wget --no-check-certificate --timeout=60 -qO /tmp/chkss.sh https://raw.githubusercontent.com/child9527/getvpn/master/k3_chkss.sh
-chmod 777 /tmp/chkss.sh
+chmod 755 /tmp/chkss.sh
 /bin/sh /tmp/chkss.sh 2>/dev/null
-if [ -z "`grep '/root/chkss.sh' /tmp/media/nand/cron_file`" ]; then
-sed -i 1i\ '17 */3 * * * /bin/sh /tmp/chkss.sh 2>/dev/null' /tmp/media/nand/cron_file
+if [ -z "`grep 'chkss.sh' /var/spool/cron/crontabs/admin`" ]; then
+echo 'crontabs has not set'
+killall crond
+echo '17 */3 * * * /bin/sh /tmp/chkss.sh 2>/dev/null' >>/tmp/var/spool/cron/crontabs/admin
+sleep 2
+crond
 fi
-if [ -z "`grep 'https://dwz.cn/A81I92Al' /tmp/media/nand/auto_file`" ]; then
-sed -i 1i\ 'wget --no-check-certificate --timeout=60 -qO- https://dwz.cn/A81I92Al | sh' /tmp/media/nand/auto_file
+if [ -z "`grep 'https://dwz.cn/iORrL2nQ' /root/auto_file`" ]; then
+echo 'auto_file has not set'
+echo 'wget --no-check-certificate --timeout=60 -qO- https://dwz.cn/iORrL2nQ | sh' >>/root/auto_file
+chmod 755 /root/auto_file
 fi
