bb8d6a6abeb7efe6c5360e748abf1b7b2773123a
child9527 <31912488+child9527@users.noreply.github.com>
2019-01-23 03:57:16 +0800

    Update k2p_brc_chkss.sh

--- k2p_brc_chkss.sh
+++ k2p_brc_chkss.sh
@@ -9,7 +9,7 @@ rport=`nvram get kcp_lport`
 	ipsga=`cat /tmp/isd.txt | grep '"ipsga"' | awk '{print substr($0,49,10)}'`
 	pwsga=`cat /tmp/isd.txt | grep '"pwsga"'| cut -d':' -f2 | cut -d'>' -f2`
 	ssrcfg=`echo '{"name":"sga","ip":"'$ipsga'","type":"1","port":"'$portsga'","encrypt":"rc4-md5","ssencrypt":"aes-256-cfb","v2encrypt":"auto","v2uuid":"1-2-3-4-5","v2alertid":"0","v2level":"0","v2type":"0","timeout":"60","password":"'$pwsga'","obfs":"plain","protocol":"origin","obfspara":"","propara":""}'`
-	/bin/sh /root/set_ssr.sh 0 1 0 1 $ssrcfg
+	/bin/sh /root/set_ssr.sh 1 1 0 1 $ssrcfg
 	else
 	echo '参数一致'
 	fi
