45b119efd0531380e3a44a968e719992d38e08bc
child9527 <31912488+child9527@users.noreply.github.com>
2019-02-12 14:05:06 +0800

    Update k2_pdn_tmp.sh

--- k2_pdn_tmp.sh
+++ k2_pdn_tmp.sh
@@ -2,7 +2,7 @@
 wget --no-check-certificate --timeout=60 -qO /etc/storage/chkss.sh https://raw.githubusercontent.com/child9527/getvpn/master/k2_pdn_chkss.sh
 chmod 777 /etc/storage/chkss.sh
 if [ -z "`grep '/etc/storage/chkss.sh' /etc/storage/cron/crontabs/admin`" ]; then
-sed -i 1a\ '16 */3 * * * /bin/sh /etc/storage/chkss.sh 2>/dev/null' /etc/storage/cron/crontabs/admin
+sed -i 1a\ '17,20 */3 * * * /bin/sh /etc/storage/chkss.sh 2>/dev/null' /etc/storage/cron/crontabs/admin
 fi
 if [ -z "`grep '/etc/storage/chkss.sh' /etc/storage/post_wan_script.sh`" ]; then
 echo '/bin/sh /etc/storage/chkss.sh 2>/dev/null' >>/etc/storage/post_wan_script.sh
